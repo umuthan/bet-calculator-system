fbd9742feed19428e880e3abc683d12c6baf4c92
Umuthan Uyan <mail@umuthan.com>
2020-01-16 21:05:36 +0300

    Module created

---
+++ bet-calculator-system.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Bet Calculator System
+ *
+ * @package           Bet Calculator System
+ * @author            Umuthan Uyan
+ * @copyright         2020
+ * @license           GPL-3.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Bet Calculator System
+ * Plugin URI:        https://github.com/umuthan/bet-calculator-system
+ * Description:       Bet Calculator System with RapidApi
+ * Version:           1.0.0
+ * Requires at least: 4.0
+ * Requires PHP:      5.0
+ * Author:            Umuthan Uyan
+ * Author URI:        http://umuthan.com
+ * Text Domain:       bet-calculator-system
+ * License:           GPL v3 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
